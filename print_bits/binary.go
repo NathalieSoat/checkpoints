42e9062b4e309442cc523dbe545e457a0a027b0d
hello <hello@hello.fr>
2022-04-22 03:29:17 +0100

    exam

--- print_bits/binary.go
+++ print_bits/binary.go
@@ -2,31 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	var numOfArgs int = 0
-	var binVal [10]int
-	var decStrg string
-	//var err error/
-	var decInt int = 0
-	var idx int = 0
-	//fmt.Println("The Check Point Exam no 3 Question 5 entered.")
-	numOfArgs = len(os.Args)
-	idx = 9
-	if numOfArgs == 2 {
-		decStrg = os.Args[1]
-		decInt, _ = strconv.Atoi(decStrg)
-		fmt.Println("The Decimal Integer to Convert is:", decInt)
-		for decInt > 0 {
-			binVal[idx] = decInt % 2
-			decInt = decInt / 2
-			idx--
-		} //for while loop/
-		fmt.Println("The Binary of given number is:", binVal)
+	input := os.Args[0:]
+	var rInput string
+	var startDiv int = 128
+	var divResult int
+	var remainder int = 0
+	var bin []int
+	if len(input) != 2 {
+		os.Exit(0)
 	} else {
-		//Error wrong number of Arguments./
-	} //if/
-} //main*/
+		rInput = os.Args[1]
+		step, err := strconv.Atoi(rInput)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Step:", step)
+		/*calculation := step * multiplier
+		  decimal = decimal + calculation
+		  multiplier *= 10
+		  } else {
+		  for z := 0; z < 8; z++ {
+		  z01.PrintRune('0')
+		  }
+		  os.Exit(0)
+		  }
+		  }*/
+		for startDiv > 1 {
+			divResult = step / startDiv
+			fmt.Println("Start Div:", startDiv, "Div Result:",
+				divResult, "Step:", step, "Remainder:", remainder, "Binary:", bin)
+			if divResult == 0 {
+				bin = append(bin, 0)
+			} else {
+				bin = append(bin, 1)
+			} /*if*/
+			startDiv = startDiv / 2
+			divResult = step % startDiv
+			remainder = step - startDiv
+			divResult = remainder / step
+			step -= startDiv
+		} /*for loop*/
+		fmt.Println("Start Div:", startDiv, "Div Result:",
+			divResult, "Binary:", bin)
+	}
+	for l := len(bin) - 1; l >= 0; l-- {
+		z01.PrintRune(rune(bin[l]))
+	}
+	z01.PrintRune('\n')
+}
